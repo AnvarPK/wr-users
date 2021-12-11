bd9a5c0abfb39e33733a9514ee44dd1456ff7e62
Anvar P K <anvar.pk@quest-global.com>
2021-12-11 20:07:11 +0530

    Login styles added

--- src/pages/login/style.js
+++ src/pages/login/style.js
@@ -1,3 +1,12 @@
 export const styles = {
-    
-}
+  wrapper: {
+    marginTop: 8,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    m: 1,
+    bgcolor: "secondary.main",
+  },
+};
