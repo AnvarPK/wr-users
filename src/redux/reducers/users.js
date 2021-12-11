96ca857ddf3e381851e7edd92848725f87f0dc29
Anvar P K <anvar.pk@quest-global.com>
2021-12-12 00:09:35 +0530

    created user integrated with localStorage

--- src/redux/reducers/users.js
+++ src/redux/reducers/users.js
@@ -1,11 +1,14 @@
 import { getLocalUsers } from "../../helper/getLocal";
+import { setLocalUsers } from "../../helper/setLocal";
 import { ADD_USER, SET_USERS } from "../consts";
 
 export const userReducer = (state = [], action) => {
   const { type, payload } = action;
   switch (type) {
     case ADD_USER:
-      return [...state, { user: payload }];
+      state = [...state, { user: payload }];
+      setLocalUsers(state);
+      return state;
     case SET_USERS:
       return [...payload];
     default:
